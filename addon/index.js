1e13cb5306f2151c496bfa87a6b08af6ccdfe9c1
Sam Selikoff <sam.selikoff@gmail.com>
2015-04-17 09:16:24 -0400

    Association base class

--- addon/index.js
+++ addon/index.js
@@ -3,12 +3,20 @@ import Response from './response';
 import Sequence from './attributes/sequence';
 import Lazy from './attributes/lazy';
 import faker from './faker';
+import HasMany from './orm/associations/has-many';
+import BelongsTo from './orm/associations/belongs-to';
 
 export { faker };
 
 export default {
   Factory: Factory,
   Response: Response,
+  hasMany: function(type) {
+    return new HasMany(type);
+  },
+  belongsTo: function(type) {
+    return new BelongsTo(type);
+  },
   sequence: function(def) {
     return new Sequence(def);
   },
