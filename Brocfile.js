e8c8ef8bc57f8099b6c9fe269ea7f8f63b7fdbda
Sam Selikoff <sam.selikoff@gmail.com>
2015-04-26 00:48:33 -0400

    plugging away on belongsTo

--- Brocfile.js
+++ Brocfile.js
@@ -13,4 +13,9 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 var app = new EmberAddon();
 
+if (app.env !== 'production') {
+  app.import(app.bowerDirectory + '/sinonjs/sinon.js', { type: 'test' } );
+  // app.import('vendor/sinon-qunit.js', { type: 'test' } );
+}
+
 module.exports = app.toTree();
